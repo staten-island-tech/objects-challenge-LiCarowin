81d69a8f30686afbec3df2fb08637eb17ff12cad
LiCarowin <carolyn.lee23@sitechhs.com>
2020-04-07 08:38:10 -0400

    umm

--- app.js
+++ app.js
@@ -16,10 +16,29 @@ const presidents = [
 //Challenge Questions
 //1) Filter all presidents, leaving only the Democratic ones
 
+/* const dPresidents = presidents.filter(function(president){
+return president.party == "Democratic";
+}); */
+
 //2)Filter all presidents to leave only one term Republican presidents HINT use If statement
+
+/* const oneTerm = presidents.filter(function(president){
+  if (president.party == "R"){
+    return presidents.terms == "1";
+  }
+}); */
 
 //3) return only the last three presidents
 
+/* const lastThree = presidents.slice (1, 3);
+console.log(lastThree); */
+
 //4) log all dems who served 2 terms. HINT use chain filter, filter and slice
+
+const twoDem =  presidents.filter(function(president) {
+  if (president.party == "D"){
+  return president.terms == "2";
+  }
+});
 
 //BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done"
---
+++ notes.js
@@ -0,0 +1,41 @@
+////Objects
+/* const Omor = {
+    name: "Omor",
+    dob: 2003,
+    graduated: false,
+    age: function() {
+        console.log(this);
+        return 2020 = this.dob;
+    }
+}; */
+
+/* const Homer = {
+    name:"Homer",
+    age: 32,
+    family: {
+        wife:{
+            name:"Marge",
+            age: 32,
+        },
+        son:{
+            name:"Bart",
+            age: 10,
+        }
+    },
+    pets: ["Cat", "Dog"]
+} */
+
+////////////Deconstructuring
+
+/* const Sandra = {
+    name: "Sandra", 
+    age : 17,
+    student: true, 
+    year: "junior",
+    graduated: false
+};
+
+const {name, year} =  Sandra;
+//const student = {};
+//student.name = Sandra.name;
+const student = Sandra; //this is not a copy */
